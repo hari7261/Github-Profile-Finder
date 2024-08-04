3170612bbc766e8e4a87a92b216983ae75652fa7
HARIOM KUMAR PANDIT <138400713+hari7261@users.noreply.github.com>
2024-08-05 00:08:46 +0530

    Add files via upload

---
+++ Github-lookup/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      Made with ❤ <a href="https://github.com/hari7261"> Hariom Pandit</a>
+    </div>
+  );
+};
+
+export default Footer;
